94210f7718b804f5869bec9db2684cc5252afc2e
paulparoczai <paulparoczai@gmail.com>
2024-11-12 13:19:02 -0800

    companion draft 1

--- scripts/presets/BufferPresets.js
+++ scripts/presets/BufferPresets.js
@@ -32,6 +32,67 @@ export const BufferPresets =
         return buffer;
     },
 
+    impulse(siblingContext)
+    {
+        let IS = siblingContext;
+
+        let buffer = IS.createBuffer(1, 1);
+
+        buffer.impulse().fill();
+
+        return buffer;
+    },
+
+    randomFMPercussion(siblingContext)
+    {
+        let IS = siblingContext;
+
+        let buffer = IS.createBuffer(1, 1);
+
+        let carrierFrequency = IS.randomFloat(100, 500);
+        let modulatorFrequency = IS.randomFloat(20, 10);
+        let modulationGain = IS.randomFloat(1, 10);
+
+        buffer.frequencyModulatedSine(carrierFrequency, modulatorFrequency, modulationGain).fill();
+
+        let rampPeakPercent = IS.randomFloat(0.005, 0.015);
+        let upExp = IS.randomFloat(0.005, 0.2);
+        let downExp = IS.randomFloat(3, 6);
+        buffer.ramp(0, 1, rampPeakPercent, rampPeakPercent, upExp, downExp).multiply();
+
+        return buffer;
+    },
+
+    randomFMPad(siblingContext, nLayers = 10)
+    {
+        let IS = siblingContext;
+
+        let buffer = IS.createBuffer(1, 20);
+        let tempBuffer = IS.createBuffer(1, 20);
+
+        for(let layer = 0; layer < nLayers; layer++)
+        {
+            let carrierFrequency = IS.randomFloat(500, 7000);
+            let modulatorFrequency = IS.randomFloat(500, 7000);
+            let modulationGain = IS.randomFloat(1, 300);
+
+            tempBuffer.frequencyModulatedSine(carrierFrequency, modulatorFrequency, modulationGain).fill();
+
+            let rampPeakPercent = IS.randomFloat(0.3, 0.8);
+            let upExp = IS.randomFloat(1, 2);
+            let downExp = IS.randomFloat(1, 2);
+            let startPercent = IS.randomFloat(0, 0.4);
+            let endPercent = IS.randomFloat(startPercent, 1);
+            tempBuffer.ramp(startPercent, endPercent, rampPeakPercent, rampPeakPercent, upExp, downExp).multiply();
+
+            tempBuffer.constant(1 / nLayers).multiply();
+
+            buffer.addBuffer(tempBuffer);
+        }
+
+        return buffer;
+    },
+
     noiseStrike(siblingContext)
     {
         let IS = siblingContext;
@@ -44,20 +105,20 @@ export const BufferPresets =
         return buffer;
     },
 
-    oceanWaves(siblingContext)
+    oceanWaves(siblingContext, nChannels = 1, length = 1)
     {
         let IS = siblingContext;
 
-        let buffer = IS.createBuffer(1, 1);
+        let buffer = IS.createBuffer(nChannels, length);
 
         // Create noiseBuffer
-        let noiseBuffer = IS.createBuffer(1, 1);
+        let noiseBuffer = IS.createBuffer(nChannels, length);
 
         noiseBuffer.noise().fill();
         noiseBuffer.inverseSawtooth(4).multiply();
 
         // Create fmBuffer
-        let fmBuffer = IS.createBuffer(1, 1);
+        let fmBuffer = IS.createBuffer(nChannels, length);
         fmBuffer.frequencyModulatedSine(IS.randomFloat(2, 6), IS.randomFloat(2, 6), IS.randomFloat(1, 3)).fill();
 
         let rampUpEnd = IS.randomFloat(0.05, 0.15);
@@ -71,7 +132,7 @@ export const BufferPresets =
         fmBuffer.multiplyBuffer(noiseBuffer);
 
         // Create lowNoise
-        let lowNoise = IS.createBuffer(1, 1);
+        let lowNoise = IS.createBuffer(nChannels, length);
         lowNoise.noise().fill();
         lowNoise.ramp(0, 1, 0.3, 0.4, 3, 3).multiply();
         lowNoise.constant(0.125).multiply();
