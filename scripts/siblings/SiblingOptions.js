bf2d1a4be8c91afac49db9e6f3588643470e705c
paulparoczai <paulparoczai@gmail.com>
2025-03-19 12:03:48 -0700

    reduce options

--- scripts/siblings/SiblingOptions.js
+++ scripts/siblings/SiblingOptions.js
@@ -9,11 +9,13 @@ class SiblingOption
 
 export const SIBLING_OPTIONS_ARRAY =
 [
+    /*
     new SiblingOption("Companion", "Companion"),
     new SiblingOption("RollForHope", "roll for hope"),
     new SiblingOption("InkblotSentiment", "Inkblot Sentiment"),
     new SiblingOption("OneNote", "One Note"),
     new SiblingOption("RollingSine", "Rolling Sine"),
     new SiblingOption("SalineAnatomy", "Saline Anatomy"),
+    */
     new SiblingOption("JustForYou", "just for you")
 ]
