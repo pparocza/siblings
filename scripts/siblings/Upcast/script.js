e0bd31a9a690eaf1405ff3f6875dca638761784f
paulparoczai <paulparoczai@gmail.com>
2025-05-09 11:26:38 -0700

    start chopping upcast

--- scripts/siblings/Upcast/script.js
+++ scripts/siblings/Upcast/script.js
@@ -3,70 +3,21 @@ var piece;
 
 setTimeout(function(){bufferLoaded();}, 1000);
 
-function bufferLoaded(){
+function bufferLoaded()
+{
 
 	piece = new Piece();
 	piece.initMasterChannel();
 	piece.initFXChannels();
 	piece.load();
-
-	// INITIALIZATIONS
-
-	if(onlineButton.innerHTML == "online"){
-		setTimeout(function(){onlineBufferLoaded();}, 1000);
-	}
-
-	else if(onlineButton.innerHTML == "offline"){
-		offlineBufferLoaded();
-	}
-
 }
 
-//--------------------------------------------------------------
-
-function runPatch(){
-
+function runPatch()
+{
 		piece.start();
-
 }
 
-//--------------------------------------------------------------
-
-function stopPatch(){
-
+function stopPatch()
+{
 	piece.stop();
-
-	if(onlineButton.innerHTML=="offline"){
-		offlineBuffer.stop();
-	}
-
-}
-
-//--------------------------------------------------------------
-
-function onlineBufferLoaded(){
-
-	startButton.disabled = false;
-	startButton.innerHTML = "start";
-
-}
-
-//--------------------------------------------------------------
-
-function offlineBufferLoaded(){
-
-	runPatch();
-
-	audioCtx.startRendering().then(function(renderedBuffer){
-
-		offlineBuffer = onlineCtx.createBufferSource();
-		offlineBuffer.buffer = renderedBuffer
-
-		startButton.disabled = false;
-		startButton.innerHTML = "start";
-
-		offlineBuffer.connect(onlineCtx.destination);
-
-	})
-
 }
