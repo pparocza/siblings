817d58b4db05c75e5064ffa4ede5b6cca3c88300
paulparoczai <paulparoczai@gmail.com>
2025-05-22 13:42:07 -0700

    removed unused siblings from options

--- scripts/siblings/SiblingOptions.js
+++ scripts/siblings/SiblingOptions.js
@@ -17,10 +17,6 @@ export const SIBLING_OPTIONS_ARRAY =
     new SiblingOption("JustForYou", "just for you"),
     new SiblingOption("LivingInTheMouth", "living in the mouth"),
     new SiblingOption("RollForHope", "roll for hope"),
-    /*
-    new SiblingOption("InkblotSentiment", "inkblot sentiment"),
-    new SiblingOption("SalineAnatomy", "saline anatomy"),
-    */
     new SiblingOption("TargetEqualsBlank", "target equals blank"),
     new SiblingOption("Upcast", "upcast")
 ]
