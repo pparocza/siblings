e2e9eb1072cdc29d161bbdb93740882b4f0ec4a6
paulparoczai <paulparoczai@gmail.com>
2025-05-22 18:08:46 -0700

    flaxen visualizer

--- scripts/siblings/Flaxen/visualizer.js
+++ scripts/siblings/Flaxen/visualizer.js
@@ -1,5 +1,18 @@
 import { IS_Visualizer } from "../../visualizer/IS_Visualizer";
 
 let networkVisualizer = IS_Visualizer.visualizer.Network;
-
 IS_Visualizer.visualizer = networkVisualizer.visualize;
+
+function cool()
+{
+	networkVisualizer.showConnections = false;
+	networkVisualizer.nodeHeight = 1;
+	networkVisualizer.nodeWidth = 1;
+	networkVisualizer.nodeScale = 2.5;
+
+	networkVisualizer.columnSpacing = 1.15;
+
+	networkVisualizer.yPosition = 1.5;
+}
+
+cool();
