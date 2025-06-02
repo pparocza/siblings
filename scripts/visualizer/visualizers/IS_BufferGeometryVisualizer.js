9ede0dc14a1a10de382e70af3cd075fcd1f3dc8b
paulparoczai <paulparoczai@gmail.com>
2025-06-02 11:31:28 -0700

    delete unused visualizers

--- scripts/visualizer/visualizers/IS_BufferGeometryVisualizer.js
+++
@@ -1,8 +0,0 @@
-// TODO: sort out addons with CDN: https://threejs.org/manual/#en/fundamentals <- at the botttom
-
-export const IS_BufferGeometryVisualizer =
-{
-	visualize() {},
-
-	initialize() {}
-}
--- scripts/visualizer/visualizers/IS_Visualizers.js
+++ scripts/visualizer/visualizers/IS_Visualizers.js
@@ -1,15 +1,9 @@
-import { IS_MeshVisualizer } from "./IS_MeshVisualizer.js";
-import { IS_WireFrameVisualizer } from "./IS_WireFrameVisualizer.js";
-import { IS_BufferGeometryVisualizer } from "./IS_BufferGeometryVisualizer.js";
 import { IS_HSLVisualizer } from "./IS_HSLVisualizer.js";
 import { IS_LineVisualizer } from "./IS_LineVisualizer.js";
 import { IS_NetworkVisualizer } from "./IS_NetworkVisualizer.js"
 
 // TODO: Visualizer selection menu
 
-export const Mesh = new IS_MeshVisualizer();
-export const WireFrame = IS_WireFrameVisualizer;
-export const BufferGeometry = IS_BufferGeometryVisualizer;
 export const Line = IS_LineVisualizer;
 export const Network = IS_NetworkVisualizer;
 export const HSL = IS_HSLVisualizer;
