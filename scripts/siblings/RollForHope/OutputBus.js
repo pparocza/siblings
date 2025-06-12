f6fdd30db24042a25fc6eeed76c7cf84374d48c1
paulparoczai <paulparoczai@gmail.com>
2025-06-12 11:09:00 -0700

    remove delay highpass

--- scripts/siblings/RollForHope/OutputBus.js
+++ scripts/siblings/RollForHope/OutputBus.js
@@ -5,8 +5,8 @@ export const OutputBus =
 {
     Initialize()
     {
-        IS.connect.series(this.Reverb, this.DelayHighPass, this.MainOutput);
-        IS.connect.series(this.DelayHighPass, this.Delay, this.MainOutput);
+        IS.connect.series(this.Reverb, this.MainOutput);
+        IS.connect.series(this.Delay, this.MainOutput);
 
         this.ConfigureGridOutputBus();
         this.ConfigureKeyOutputBus();
